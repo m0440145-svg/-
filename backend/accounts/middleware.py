868ac022805ab9467b4e3d975f43c08d7144c681
m0440145-svg <m0440145@gmail.com>
2026-09-10 11:45:55 +0300

    إضافة مشروع Django: backend/accounts/middleware.py

---
+++ backend/accounts/middleware.py
@@ -0,0 +1,8 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+class ForcePasswordChangeMiddleware:
+ def __init__(self,get_response):self.get_response=get_response
+ def __call__(self,request):
+  allowed={reverse("login"),reverse("logout"),reverse("password_change"),reverse("password_change_done")}
+  if request.user.is_authenticated and request.user.must_change_password and request.path not in allowed and not request.path.startswith("/static/"):return redirect("password_change")
+  return self.get_response(request)
